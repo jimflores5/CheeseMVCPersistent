d95c301a30d21f873c7cfbe9565bb04e53b1dc64
Jim Flores <jimflores5@gmail.com>
2017-12-18 12:22:28 -0600

    Completed part 2 of the CheeseMVC Persistent assignment.

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,14 +1,17 @@
-﻿namespace CheeseMVC.Models
+﻿using System.Collections.Generic;
+
+namespace CheeseMVC.Models
 {
     public class Cheese
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public CheeseType Type { get; set; }
-        public int Rating { get; set; }
+        //public int Rating { get; set; }
         public int ID { get; set; }
 
         public int CategoryID { get; set; }  //This represents the 'foreign key' that allows us to relate each cheese to a category.
         public CheeseCategory Category { get; set; } //Navigation property corresponding to the foreign key.
+
+        public List<CheeseMenu> CheeseMenus { get; set; }
     }
 }
